a1fe01d36cd9397c0418b7d047b6e0194189bd32
Alejandra Valdivia <alexvcodes@gmail.com>
2024-07-17 22:05:29 -0700

    feat: add logo, wireframe, dashboard, and navigation menu

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Front End
 
 Application to help users find and hire people within their profession to help with tasks at their home such furniture assembly, mounting, cleaning, painting, gardening, home repairs and more.
+
+## ERM (Entity Relationship Diagram)
+
+![Taskify Logo](./src/assets/taskify-app.jpg)
+![Taskify Wireframe](./src/assets/taskify-wireframe-v1.png)
+
---
+++ src/components/BookTaskBtn/BookTaskBtn.jsx
@@ -0,0 +1,31 @@
+import { Button } from "react-bootstrap";
+import { useNavigate, useParams } from "react-router-dom";
+
+
+
+
+
+
+const BookTaskBtn = (props) => {
+    const { taskId } = useParams();
+    const navigate = useNavigate();
+    const singleTask = props.task?.find((task) => task?._id === Number(taskId));
+    console.log("BookTaskBtn: singleTask", singleTask);
+    if (!singleTask) {
+        return <p>Task not found.</p>;
+    }
+
+    return (
+        <>
+            <Button
+                variant="primary"
+                onClick={() => {
+                    navigate(`/task/${singleTask._id}/book`);
+                }}
+            >
+                Book Task
+            </Button>
+        </>
+    );
+};
+export default BookTaskBtn;
