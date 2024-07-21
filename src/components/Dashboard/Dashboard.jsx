5e9c97fb146e281a1b7157c48e3e99f0c2962fb4
Alejandra Valdivia <alexvcodes@gmail.com>
2024-07-20 21:38:47 -0700

    feat: add services gallery to the dashboard component

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,20 +1,98 @@
+import { Container, Row, Col } from "react-bootstrap";
+
 import { Link } from "react-router-dom";
 import TaskList from "../TaskList/TaskList";
+import "../TaskList/TaskList.css";
+import "./Dashboard.css";
 const Dashboard = (props) => {
   return (
     <>
-      <div className="dashboard">
+      <Link to="/dashboard">
         <h1>Dashboard</h1>
-        <Link to="/tasks">View Tasks</Link>
-        <TaskList tasks={props.tasks} onClick={props.bookTask} />
+      </Link>
 
-        <div className="dashboard-img">
-          <img
-            src="/imhttps://i.imgur.com/apDRyEJ.jpeg/theme-photos-Klby0nxseY8-unsplash.jpg."
-            alt="drill"
-          />
+      <div className="dashboard-links">
+        <Link className="dashboard-link" to="/">
+          Home
+        </Link>
+        <Link className="dashboard-link" to="/account">
+          Account
+        </Link>
+        <Link className="dashboard-link" to="/task/new">
+          Add New Task
+        </Link>
+        <Link className="dashboard-link" to="/tasks">
+          View Tasks
+        </Link>
+      </div>
+      <div className="dashboard-content">
+        <h2>Popular Projects</h2>
+        <div id="dashboard-img-gallery">
+          <div className="dashboard-img">
+            <img
+              src="public/img/AdobeStock_189916326.jpeg"
+              alt="man assembling furniture"
+            />
+            <h3>Furniture Assembly</h3>
+            <p>Projects starting at $47</p>
+          </div>
+          <div className="dashboard-img">
+            <img
+              src="public/img/robinson-greig-HrnAxAUwle8-unsplash.jpg"
+              alt="men loading a truck"
+            />
+            <h3>Help Moving</h3>
+            <p>Projects starting at $56</p>
+          </div>
+          <div className="dashboard-img">
+            <img
+              src="public/img/theme-photos-Klby0nxseY8-unsplash.jpg"
+              alt="drill"
+            />
+            <h3>Home Repairs</h3>
+            <p>Projects starting at $47</p>
+          </div>
+          <div className="dashboard-img">
+            <img src="public/img/AdobeStock_320287361.jpeg" alt="drill" />
+            <h3>Home Cleaning</h3>
+            <p>Projects starting at $40</p>
+          </div>
+          <div className="dashboard-img">
+            <img
+              src="public/img/AdobeStock_718073342.jpeg"
+              alt="man fixing a leaky faucet"
+            />
+            <h3>Plumbing Repairs</h3>
+            <p>Projects starting at $67</p>
+          </div>
+          <div className="dashboard-img">
+            <img
+              src="public/img/AdobeStock_636880936.jpeg"
+              alt="electrician fixing a circuit"
+            />
+            <h3>Electrical Help</h3>
+            <p>Projects starting at $67</p>
+          </div>
+          <div className="dashboard-img">
+            <img
+              src="public/img/AdobeStock_516534949.jpeg"
+              alt="men carrying a couch"
+            />
+            <h3>Heavy Lifting</h3>
+            <p>Projects starting at $54</p>
+          </div>
+          <div className="dashboard-img">
+            <img
+              src="public/img/AdobeStock_593389323.jpeg"
+              alt="gardener trimming shrubs"
+            />
+            <h3>Yard Word & Gardeing</h3>
+            <p>Projects starting at $44</p>
+          </div>
         </div>
       </div>
+
+      <TaskList tasks={props.tasks} onClick={props.bookTask} />
     </>
   );
 };
--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,13 +1,19 @@
-
+import "./Homepage.css";
 const Homepage = () => {
   return (
     <>
       <div>
         <h1 className="tagline">Taskify: Quick Fixes, Expert Solutions.</h1>
-        <p>
+        <h2>
           Helping you find and hire people within your profession to help with
           tasks at your home.
-        </p>
+        </h2>
+        <div className="homepage-image-container">
+          <img
+            src="public/img/AdobeStock_418746653.jpeg"
+            alt="man overwhelmed with furniture assembly instructions"
+          />
+        </div>
       </div>
     </>
   );
