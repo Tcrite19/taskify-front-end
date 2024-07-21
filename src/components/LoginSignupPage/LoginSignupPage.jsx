9b7c24594178782a812d7047334baecbe2e5c764
Alejandra Valdivia <alexvcodes@gmail.com>
2024-07-20 23:05:17 -0700

    feat: add LoginSignupPage component

---
+++ src/components/LoginSignupPage/LoginSignupPage.jsx
@@ -0,0 +1,14 @@
+import "./LoginSignupPage.css";
+
+const LoginSignupPage = (props) => {
+  return (
+    <div className="login-signup-page" alt="man fixing a leaky faucet">
+      <div className="login-signup-card">
+        <a href="/login" className="login">Login</a>
+        <a href="/signup" className="signup">Sign Up</a>
+        <p className="p-text-margin">By signing up you agree to our Terms of Use and Privacy Policy.</p>
+      </div>
+    </div>
+  );
+};
+export default LoginSignupPage;
