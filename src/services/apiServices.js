8a051c07d810e35d917437ab8a5bfd2ddd3ec1c8
Alejandra Valdivia <alexvcodes@gmail.com>
2024-07-26 12:38:47 -0700

    feat: add renderered app link to README.md file, updated getTasks, getTask, addTask, updateTask, bookTask, searchTask to fetch from the BE

--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -3,72 +3,102 @@ const BACKEND_URL = import.meta.env.VITE_EXPRESS_BACKEND_URL;
 export const signup = async (user) => {
   try {
     const res = await fetch(`${BACKEND_URL}/users/signup`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(user),
-    })
-    const json = await res.json()
+    });
+    const IsParsable = (data) => {
+      try {
+        JSON.parse(data);
+      } catch (e) {
+        return false;
+      }
+      return true;
+    };
+
+    const Obj = JSON.stringify({
+      results: IsParsable(res) ? JSON.parse(res) : false,
+    });
+
+    return IsParsable(Obj) ? JSON.parse(Obj) : false;
 
     if (json.token) {
-      localStorage.setItem('token', json.token); // add this line to store the JWT token in localStorage
+      localStorage.setItem("token", json.token); // add this line to store the JWT token in localStorage
 
-      const user = JSON.parse(atob(json.token.split('.')[1]));
+      const user = JSON.parse(atob(json.token.split(".")[1]));
 
-      return user
+      return user;
     }
     if (json.err) {
-      throw new Error(json.err)
+      throw new Error(json.err);
     }
   } catch (err) {
-    console.log(err)
-    throw err
+    console.log(err);
+    throw err;
   }
-}
-
+};
 
 export const signin = async (user) => {
   try {
     const res = await fetch(`${BACKEND_URL}/users/signin`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(user),
-    })
-    const json = await res.json()
+    });
+
+    const IsParsable = (data) => {
+      try {
+        JSON.parse(data);
+      } catch (e) {
+        return false;
+      }
+      return true;
+    };
+
+    const Obj = JSON.stringify({
+      results: IsParsable(res) ? JSON.parse(res) : false,
+    });
+
+    return IsParsable(Obj) ? JSON.parse(Obj) : false;
 
     if (json.token) {
-      localStorage.setItem('token', json.token); // add this line to store the JWT token in localStorage
+      localStorage.setItem("token", json.token);
 
-      const user = JSON.parse(atob(json.token.split('.')[1]));
+      const user = JSON.parse(atob(json.token.split(".")[1]));
 
-      return user
+      return user;
     }
     if (json.err) {
-      throw new Error(json.err)
+      throw new Error(json.err);
     }
   } catch (err) {
-    console.log(err)
-    throw err
+    console.log(err);
+    throw err;
   }
-}
+};
 
-export const getUser = () =>  {
-  const token = localStorage.getItem('token');
+export const getUser = () => {
+  const token = localStorage.getItem("token");
   if (!token) return null;
-  const user = JSON.parse(atob(token.split('.')[1]));
+  const user = JSON.parse(atob(token.split(".")[1]));
   return user;
-}
+};
 
 export const signout = () => {
-  localStorage.removeItem('token');
+  localStorage.removeItem("token");
 };
 
 /* API CALLS FOR TASKS */
 
-export const getTasks = async (task) => {
+export const getTasks = async (tasks) => {
   try {
-    const response = await fetch(
-      `${BACKEND_URL}/tasks?name=${task}` || BACKEND_URL + "/tasks"
-    );
+    const response = await fetch(BACKEND_URL + "/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(tasks),
+    });
     const tasks = await response.json();
     return tasks;
   } catch (error) {
@@ -77,9 +107,15 @@ export const getTasks = async (task) => {
   }
 };
 
-export const getTask = async (id) => {
+export const getTask = async (task) => {
   try {
-    const response = await fetch(`${BACKEND_URL}/${id}`);
+    const response = await fetch(BACKEND_URL + "/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
     const task = await response.json();
     return task;
   } catch (error) {
@@ -89,56 +125,87 @@ export const getTask = async (id) => {
 };
 
 export const addTask = async (task) => {
-  const response = await fetch(BACKEND_URL + "/tasks", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(task),
-  });
-  const newTask = await response.json();
-  return newTask;
+  try {
+    const response = await fetch(BACKEND_URL + "/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+    const newTask = await response.json();
+    return newTask;
+  } catch (error) {
+    error.message = "Error fetching task";
+    throw error;
+  }
 };
 
-export const deleteTask = async (id) => {
-  const response = await fetch(`${BACKEND_URL}/${id}`, {
-    method: "DELETE",
-  });
-  const deletedTask = await response.json();
-  return deletedTask;
+export const deleteTask = async (task) => {
+  try {
+    const response = await fetch(BACKEND_URL + "/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+    const newTask = await response.json();
+    return newTask;
+  } catch (error) {
+    error.message = "Error fetching tasks";
+    throw error;
+  }
 };
 
-export const updateTask = async (id, updatedTask) => {
-  const response = await fetch(`${BACKEND_URL}/${id}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(updatedTask),
-  });
-  const updatedTaskData = await response.json();
-  return updatedTaskData;
+export const updateTask = async (task, updatedTask) => {
+  if (!task || !updatedTask) {
+    throw new Error("Error: task or updatedTask is null or undefined");
+  }
+  try {
+    const response = await fetch(BACKEND_URL + "/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+
+    const task = await response.json();
+    const updatedTask = await updateTask(task, updatedTask);
+    return updatedTask;
+  } catch (error) {
+    error.message = "Error fetching task";
+    throw error;
+  }
 };
 
 export const bookTask = (task) => {
-  if (task) {
-    console.log("App: bookTask", task);
-    const updatedTask = { ...task, booked: true };
-    setTask((prevTasks) =>
-      prevTasks.map((task) => (task._id === task._id ? updatedTask : task))
-    );
-    setBooked(true);
+  try {
+    if (task) {
+      console.log("App: bookTask", task);
+      const updatedTask = { ...task, booked: true };
+      setTask((prevTasks) =>
+        prevTasks.map((task) => (task._id === task._id ? updatedTask : task))
+      );
+      setBooked(true);
+    }
+  } catch (error) {
+    error.message = "Error fetching task";
+    throw error;
   }
 };
 
 export const searchTask = async (task) => {
   try {
-    const queryString = `?name=${task}`;
-    const response = await fetch(BACKEND_URL + queryString);
+    const response = await fetch(`${BACKEND_URL}/tasks?name=${task}`);
+    if (!response.ok) {
+      throw new Error(`Error: ${response.statusText}`);
+    }
     const tasks = await response.json();
-    return tasks;
+    return tasks.length > 0 ? tasks[0] : null;
   } catch (error) {
-    error.message = "Error fetching tasks";
+    error.message = "Error fetching task";
     throw error;
   }
 };
